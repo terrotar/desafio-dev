f8fb6b6a8dd2e82a53aedae39c57213a9b48d695
terrotar <pedro_guedes93@hotmail.com>
2021-10-22 15:59:00 -0300

    Created dir templates/login to handle better the pages of change data, profile and login; Created the function of change password

--- app/blueprints/login/routes.py
+++ app/blueprints/login/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, redirect, render_template, request
 
-from flask_login import login_user, logout_user
+from flask_login import current_user, login_user, logout_user
 
 from app.models.user import User
+
+from app.config import db
 
 
 # Instance of Blueprint login
@@ -19,12 +21,12 @@ def log_user():
         password = request.form['pwd']
         user = User.query.filter_by(email=email).first()
         if(not user or not user.verify_password(password)):
-            return render_template('login.html',
+            return render_template('login/login.html',
                                    error=True)
         else:
             login_user(user)
             return redirect('/')
-    return render_template('login.html')
+    return render_template('login/login.html')
 
 
 # logout
@@ -33,3 +35,31 @@ def logout():
     if (request.method == 'GET'):
         logout_user()
         return redirect('/')
+
+
+# Profile
+@login.route('/profile', methods=['GET'])
+def profile():
+    if(request.method == 'GET'):
+        return render_template('login/profile.html')
+
+
+# Change_pwd
+@login.route('/change/pwd', methods=['GET', 'POST'])
+def change_pwd():
+    user = User.query.get(current_user.id)
+    if(request.method == 'GET'):
+        if(user):
+            return render_template('login/change_pwd.html')
+    elif(request.method == 'POST'):
+        old_pwd = request.form['old_pwd']
+        new_pwd = request.form['new_pwd']
+        if(not user.verify_password(old_pwd)):
+            return render_template('login/login.html',
+                                   error=True)
+        else:
+            user.password = new_pwd
+            db.session.commit()
+            logout_user()
+            return render_template('index.html')
+    return render_template('login/profile.html')
